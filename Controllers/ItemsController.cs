85aa699235fd59d41fafe58191181a0de59764f7
Patrick Adutwum <adutwum.pa@gmail.com>
2022-09-21 14:51:15 +0000

    route implementation put, post and delete

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,4 +1,5 @@
 using Catalog.DTOs;
+using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,6 +30,57 @@ namespace Catalog.Controllers
                 return NotFound();
             }
             return Ok(item.AsDto());
+        }
+
+        // POST /items
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new Item(){
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id}, item.AsDto());
+        }
+
+        // PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if(existingItem is null){
+                return NotFound();
+            }
+
+            Item updatedItem = existingItem with{
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
+
+        // DELETE /item/id
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if(existingItem is null){
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+
+            return NoContent();
         }
     }
 }
